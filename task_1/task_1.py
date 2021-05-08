6f4aaad95ffbdb263d82941e2e37b9a0d027b255
Semra Ali <semriishh@abv.bg>
2021-05-08 17:25:43 +0300

    sym of 2 ints. verify if result is < 50 and check if result is odd or even

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -1,1 +1,15 @@
-print("hello world")
+a=25
+b=18
+result=a+b
+print(result)
+if result <50:
+    result = result /2
+print(result)
+
+result = int(result)
+
+if result % 2 == 0:
+    print("the result is chetno")
+
+if result % 2 == 1:
+    print("the result is nechotno")
